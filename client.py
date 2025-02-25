a0dabaeefa89c2943c6d12be615a2504f62f4217
akemashita <134128275+akemashita@users.noreply.github.com>
2025-02-25 07:22:44 +0000

    first commit

---
+++ client.py
@@ -0,0 +1,29 @@
+# client.py
+# stage1
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = input("Type in the server's address to connect to: ")
+server_port = 9001
+
+address = ""
+port = 9050
+message = b"Message to send to the client."
+
+sock.bind((address, port))
+
+try:
+    print("sending {!r}".format(message))
+
+    sent = sock.sendto(message, (server_address, server_port))
+    print("Send {} bytes".format(sent))
+
+    print("waiting to receive")
+    data, server = sock.recvfrom(4096)
+    print("received {!r}".format(data))
+
+finally:
+    print("closing socket")
+    sock.close()
---
+++ server.py
@@ -0,0 +1,24 @@
+# server.py
+# stage1
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = "0.0.0.0"
+server_port = 9001
+print("starting up on port {}".format(server_port))
+
+sock.bind((server_address, server_port))
+
+# クライアントからの接続を待つ処理
+while True:
+    print("\nwaiting to recieve message")
+    data, address = sock.recvfrom(4096)
+
+    print("received {} bytes from".format(len(data), address))
+    print(data)
+
+    if data:
+        sent = sock.sendto(data, address)
+        print("sent {} bytes back to {}".format(sent, address))
