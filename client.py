29d769ced37888c5c6db975463ae44756ca159a3
akemashita <134128275+akemashita@users.noreply.github.com>
2025-03-11 11:55:37 +0900

    refactor(client): encapsulate logic into UDPClient class for better structure.

--- client.py
+++ client.py
@@ -6,106 +6,170 @@ import threading
 import queue
 import sys
 import readline
-
-# スレッド間通信のためのキュー
-send_queue = queue.Queue()
-receive_queue = queue.Queue()
+import time
 
 
-# 送受信を担当するスレッド
-def send_receive_message(
-    client_socket, username_len, username_bytes, send_queue, receive_queue
-):
-    sock = client_socket
-    sock.setblocking(False)  # ソケットをノンブロッキングモードにする
+class UDPClient:
+    def __init__(
+        self,
+        server_address="0.0.0.0",
+        server_port=9001,
+        client_address="",
+        client_port=0,
+        username="no name",
+        username_bytes=7,
+        username_len=7,
+    ):
+        ###########
+        # 設定情報
+        ###########
+        self.server_address = server_address
+        self.server_port = server_port
+        self.client_address = client_address
+        self.client_port = client_port
+        self.username = username
+        self.username_bytes = username_bytes
+        self.username_len = username_len
+        self.thread_running = True
 
-    while True:
+        # ソケット設定
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        # クライアント側のポートは OS に自動的に割り当ててもらう
+        self.sock.bind((self.client_address, self.client_port))
+
+        # スレッド間通信のためのキュー
+        self.send_queue = queue.Queue()
+        self.receive_queue = queue.Queue()
+
+    def start(self):
+        """クライアントを起動して、メッセージの送受信を行う"""
+        # 案内を表示する
+        print("### お知らせ ###")
+        print("行頭に > が表示されているときは入力ができます。")
+        print("終了したいときは exit と入力してください。")
+
+        # 各スレッドを開始する
+        send_receive_thread = threading.Thread(
+            target=self.send_receive_message,
+            daemon=True,
+        )
+        send_receive_thread.start()
+
+        input_thread = threading.Thread(target=self.input_message, daemon=True)
+        input_thread.start()
+
+        loop = True
         try:
-            #### メッセージ送信 ####
-            if not send_queue.empty():
-                own_message = send_queue.get()
-                own_message_bytes = own_message.encode("utf-8")
+            while loop:
+                # 受信メッセージがあれば表示
+                while not self.receive_queue.empty():
+                    sender_username, receive_message = self.receive_queue.get()
+                    if sender_username == "exit" and receive_message == "exit":
+                        loop = False
+                        break
+                    else:
+                        if username != sender_username:
+                            self.print_message(sender_username, receive_message)
+                        else:
+                            self.print_message("YOU", receive_message)
 
-                # 送信データを作成（serialize）
-                send_data = bytes([username_len]) + username_bytes + own_message_bytes
-                sent = sock.sendto(send_data, (server_address, server_port))
-                # print(f"[DEBUG] send_data: {send_data}")
+        finally:
+            self.thread_running = False
+            print("\nstop threads")
+            send_receive_thread.join()
+            input_thread.join()
+            print("closing socket")
+            self.sock.close()
 
-            #### メッセージ受信 ####
+    def send_receive_message(self):
+        """送受信を担当するスレッド"""
+        self.sock.setblocking(False)  # ソケットをノンブロッキングモードにする
+
+        while self.thread_running:
             try:
-                # 受信メッセージを分解（deserialize）
-                receive_data, _ = sock.recvfrom(4096)
+                time.sleep(0.1)
 
-                if len(receive_data) < 2:
-                    continue
+                #### メッセージ送信 ####
+                if not self.send_queue.empty():
+                    own_message = self.send_queue.get()
+                    own_message_bytes = own_message.encode("utf-8")
 
-                # 最初の１バイトを sender_username_len として読み取る
-                sender_username_len = receive_data[0]
+                    # 送信データを作成（serialize）
+                    send_data = (
+                        bytes([username_len]) + self.username_bytes + own_message_bytes
+                    )
+                    sent = self.sock.sendto(
+                        send_data, (self.server_address, self.server_port)
+                    )
+                    # print(f"[DEBUG] send_data: {send_data}")
 
-                if len(receive_data) < 1 + sender_username_len:
-                    continue
+                #### メッセージ受信 ####
+                try:
+                    # 受信メッセージを分解（deserialize）
+                    receive_data, _ = self.sock.recvfrom(4096)
 
-                # 次の sender_username_len バイトがユーザ名
-                sender_username_bytes = receive_data[1 : 1 + sender_username_len]
-                sender_username = sender_username_bytes.decode("utf-8")
+                    if len(receive_data) < 2:
+                        continue
 
-                # 残りのバイトがメッセージ
-                sender_message_bytes = receive_data[1 + sender_username_len :]
-                sender_message = sender_message_bytes.decode("utf-8")
+                    # 最初の１バイトを sender_username_len として読み取る
+                    sender_username_len = receive_data[0]
 
-                receive_queue.put([sender_username, sender_message])
+                    if len(receive_data) < 1 + sender_username_len:
+                        continue
 
-            except BlockingIOError:
-                # データがないときはスルー
-                pass
+                    # 次の sender_username_len バイトがユーザ名
+                    sender_username_bytes = receive_data[1 : 1 + sender_username_len]
+                    sender_username = sender_username_bytes.decode("utf-8")
 
-        except Exception as e:
-            print(f"Error: {e}")
+                    # 残りのバイトがメッセージ
+                    sender_message_bytes = receive_data[1 + sender_username_len :]
+                    sender_message = sender_message_bytes.decode("utf-8")
 
+                    self.receive_queue.put([sender_username, sender_message])
 
-# ユーザ入力を受け付けるスレッド
-def input_message(send_queue, receive_queue):
-    while True:
-        # メッセージを入力させる
-        message = input("\n> ")
+                except BlockingIOError:
+                    # データがないときはスルー
+                    pass
 
-        # 入力行を削除してサーバから受信した行のみを表示する
-        sys.stdout.write(
-            "\033[F\033[K\033[F"
-        )  # １行上に移動、入力行をクリア、１行上に移動
-        sys.stdout.flush()
+            except Exception as e:
+                print(f"Error: {e}")
 
-        send_queue.put(message)
+    def input_message(self):
+        """ユーザ入力を受け付けるスレッド"""
+        while self.thread_running:
+            # メッセージを入力させる
+            message = input("\n> ")
 
-        if message.lower() == "exit":
-            receive_queue.put(["exit", "exit"])
-            break  # スレッドを終了
+            # 入力行を削除してサーバから受信した行のみを表示する
+            sys.stdout.write(
+                "\033[F\033[K\033[F"
+            )  # １行上に移動、入力行をクリア、１行上に移動
+            sys.stdout.flush()
 
+            self.send_queue.put(message)
 
-def print_message(sender, message):
-    sys.stdout.write("\033[K")  # 現在の行をクリア
-    print(f"\r[{sender}] {message}\n> {input_buffer()}", end="", flush=True)
+            if message.lower() == "exit":
+                self.receive_queue.put(["exit", "exit"])
+                break  # スレッドを終了
 
+    def print_message(self, sender, message):
+        """メッセージを表示して、その後に入力行を表示する"""
+        sys.stdout.write("\033[K")  # 現在の行をクリア
+        print(f"\r[{sender}] {message}\n> {self.input_buffer()}", end="", flush=True)
 
-# 入力途中の状態を取得する
-def input_buffer():
-    try:
-        return readline.get_line_buffer()
-    except ImportError:
-        return ""
+    def input_buffer(self):
+        """入力途中の状態を取得する"""
+        try:
+            return readline.get_line_buffer()
+        except ImportError:
+            return ""
 
 
 # メイン処理：受信メッセージの表示
 if __name__ == "__main__":
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
     server_address = input("Type in the server's address to connect to: ")
     server_port = 9001
-
-    # クライアント側のポートは OS に自動的に割り当ててもらう
-    address = ""
-    port = 0
-    sock.bind((address, port))
 
     # ユーザ名を入力させる
     username = input("ユーザ名を入力してください。\n> ")
@@ -116,39 +180,11 @@ if __name__ == "__main__":
         print("エラー：ユーザ名が長すぎます（最大 255 バイト）")
         exit(1)
 
-    # 入力の案内を表示する
-    print("### お知らせ ###")
-    print("行頭に > が表示されているときは入力ができます。")
-    print("終了したいときは exit と入力してください。")
-
-    # 各スレッドを開始
-    send_receive_thread = threading.Thread(
-        target=send_receive_message,
-        args=(sock, username_len, username_bytes, send_queue, receive_queue),
-        daemon=True,
+    client = UDPClient(
+        server_address=server_address,
+        server_port=server_port,
+        username=username,
+        username_bytes=username_bytes,
+        username_len=username_len,
     )
-    send_receive_thread.start()
-
-    input_thread = threading.Thread(
-        target=input_message, args=(send_queue, receive_queue), daemon=True
-    )
-    input_thread.start()
-
-    loop = True
-    try:
-        while loop:
-            # 受信メッセージがあれば表示
-            while not receive_queue.empty():
-                sender_username, receive_message = receive_queue.get()
-                if sender_username == "exit" and receive_message == "exit":
-                    loop = False
-                    break
-                else:
-                    if username != sender_username:
-                        print_message(sender_username, receive_message)
-                    else:
-                        print_message("YOU", receive_message)
-
-    finally:
-        print("\nclosing socket")
-        sock.close()
+    client.start()
